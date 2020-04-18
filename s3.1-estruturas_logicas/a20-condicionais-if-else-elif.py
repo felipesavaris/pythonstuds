d20d04ca9d187f67b68325c54ff617a169d4fab4
FelipeSavaris <fesavaris@gmail.com>
2020-04-18 16:55:48 -0300

    estrutura condicional - if, elif e else

---
+++ s3.1-estruturas_logicas/a20-condicionais-if-else-elif.py
@@ -0,0 +1,17 @@
+"""
+condições - estruturas
+
+if
+elif
+else
+"""
+
+idade = 18
+
+
+if idade < 18:
+    print('Menor de idade')
+elif idade >= 18:
+    print('Maior de idade')
+else:
+    print('Idade inválida')
