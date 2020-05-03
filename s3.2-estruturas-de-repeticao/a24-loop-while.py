297543142c78eed82a34a56882a5900141556645
FelipeSavaris <fesavaris@gmail.com>
2020-05-02 22:02:44 -0300

    loop while

---
+++ s3.2-estruturas-de-repeticao/a24-loop-while.py
@@ -0,0 +1,25 @@
+"""
+loop while
+
+while expressao_booleana; -> true ou false
+    executiotion loop
+"""
+num = 1
+
+while num < 4:
+    print(num)
+    num += 1
+#------------------------------------------------------
+
+
+parada = ''
+
+while parada != 'n':
+    input(f'{num} - Deseja executar novamente? ')
+    num += 12
+
+"""
+baixar o jogo FROZEN-BUBBLE na loja do ubuntu
+observe que as fases são executadas em while conforme vc envia a execuçao..
+o while é infinito
+"""
